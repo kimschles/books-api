7816936ad99cfa4a3185ffb4b36d285889d10ccc
coloradokim <schlesinger.kim@gmail.com>
2018-04-24 20:33:49 -0600

    add read all and read 1 routes

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
+const dbConnection = require('./connection');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/api/v1/books', (req, res) => res.send('Hello Books!'));
+app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'));
+
+app.get('/api/v1/books', (req, res) => {
+  dbConnection('book')
+    .select('*')
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(500).json({ error: error.message, stack: error.stack }));
+});
+
+app.get('/api/v1/books/:id', (req, res) => {
+  dbConnection('book')
+    .select('*')
+    .limit(1)
+    .where({ id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(500).json({ error: error.message, stack: error.stack }));
+});
 
 app.listen(PORT, () => console.log('Example app running!'));
--- seeds/00_books.js
+++ seeds/00_books.js
@@ -1,20 +1,33 @@
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex('book')
     .del()
     .then(function() {
-      // Inserts seed entries
       return knex('book').insert([
-        {
-          title: 'Wave: A Memoir of Life After the Tsunami',
-          author: 'Sonali Deraniyagala',
-          cover_image:
-            'https://images-na.ssl-images-amazon.com/images/I/31qgMqGxGXL._SY344_BO1,204,203,200_.jpg'
-        },
         {
           title: 'You Are Not a Gadget: A Manifesto',
           author: 'Jaron Lanier',
-          cover_image: 'https://images.gr-assets.com/books/1320388392l/6683549.jpg'
+          cover_image: 'https://cybject.files.wordpress.com/2013/03/jaron.jpg'
+        },
+        {
+          title: 'Radical Candor ',
+          author: 'Kim Scott',
+          cover_image: 'https://images-na.ssl-images-amazon.com/images/I/411O8D1w2UL.jpg'
+        },
+        {
+          title: 'Station Eleven',
+          author: 'Emily St. John Mandel',
+          cover_image:
+            'https://images-na.ssl-images-amazon.com/images/I/51-qQ2TbIPL._SX323_BO1,204,203,200_.jpg'
+        },
+        {
+          title: 'Just the Funny Parts',
+          author: 'Nell Scovell',
+          cover_image: 'https://i.harperapps.com/covers/9780062473509/y450-293.jpg'
+        },
+        {
+          title: 'Wave: A Memoir of Life After the Tsunami',
+          author: 'Sonali Deraniyagala',
+          cover_image: 'https://images-na.ssl-images-amazon.com/images/I/31sWoadXnhL.jpg'
         }
       ]);
     });
